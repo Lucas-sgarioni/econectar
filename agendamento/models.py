5da91550503ef3b341a25451293dcd290b91e522
Guilherme-Monteiro-de-Medeiros <gm.medeiros6@gmail.com>
2025-05-18 14:17:48 -0300

    Update models.py

--- agendamento/models.py
+++ agendamento/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Agendamento(models.Model):
+  nome = models.Charfield(verbose_name= 'Nome Completo', max_length=70)
+  email = models.EmailField(verbose_name= 'E-mail')
+  endereco = models.CharField(verbose_name= 'Endereço', max_length=100)
+  data = models.DateField(verbose_name= 'Data de Coleta')
+  hora = models.TimeField(verbose_name= 'Hora da Coleta')
+  observacao = models.CharFfield(verbose_name= 'Observação', max _length=999)
+  
+
 # Create your models here.
